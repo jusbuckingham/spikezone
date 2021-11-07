69f97ceccdc1e728f4e6c8cc0913dae0946f07c2
jusbuckingham <jus.buckingham@gmail.com>
2021-11-07 14:42:40 -0800

    added some questions

---
+++ main.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        title: "What was the character’s name that Denzel Washington played in Mo Better Blues?:",
+        choices: ["Moses", "Bleek", "Shadow", "Mookie"],
+        answer: "Bleek"
+    },
+    {
+        title: "What is the name of Mos Def’s character in Bamboozled?",
+        choices: ["Big Black Africa", "Lenny", "Buddy", "Dante"],
+        answer: "Big Black Africa"
+    },
+    {
+        title: "The name of the pizzeria Mookie worked at in Do The Right Thing?",
+        choices: ["Lemmy's", "Jersey Steven's", "Pizza Hut", "Sal's Famous"],
+        answer: "Sal's Famous"
+    },
+    {
+        title: "David Byrne and Spike Lee collaborated on what live filmed version of a broadway hit?",
+        choices: ["Hamlet", "American Idiot", "American Utopia", "Annie"],
+        answer: "American Utopia"
+    },
+    {
+        title: "Who starred in Clockers?",
+        choices: ["Denzel Washington", "Omar Epps", "Bill Murray", "Mekhi Phifer"],
+        answer: "Mekhi Phifer"
+    },
+//     {
+//         title: "Undefined does NOT mean that _______.",
+//         choices: ["the variable in the code doesn't exist", "the variable is not assigned to a value", "the variable is assigned no value", "the property doesn't exist"],
+//         answer: "the variable is assigned no value"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ______.",
+//         choices: ["strings", "numbers", "objects", "All of the Above"],
+//         answer: "All of the Above"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ______.",
+//         choices: ["strings", "numbers", "objects", "All of the Above"],
+//         answer: "All of the Above"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ______.",
+//         choices: ["strings", "numbers", "objects", "All of the Above"],
+//         answer: "All of the Above"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ______.",
+//         choices: ["strings", "numbers", "objects", "All of the Above"],
+//         answer: "All of the Above"
+//     },
+//     {
+//         title: "Arrays in JavaScript can be used to store ______.",
+//         choices: ["strings", "numbers", "objects", "All of the Above"],
+//         answer: "All of the Above"
+//     }
+// ];
