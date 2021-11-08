438d43d5936a9acf4cf9b2a5b4da234dcb779ba2
jusbuckingham <jus.buckingham@gmail.com>
2021-11-08 15:59:04 -0800

    playing with the styling again

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const Questions = [{
     id: 0,
     q: "What's the character's name Denzel Washington plays in Mo Better Blues?",
     a: [{ text: "Moses", isCorrect: false },
-        { text: "Shadow", isCorrect: false },
-        { text: "Bleek", isCorrect: true },
-        { text: "Mookie", isCorrect: false }
+    { text: "Shadow", isCorrect: false },
+    { text: "Bleek", isCorrect: true },
+    { text: "Mookie", isCorrect: false }
     ]
 
 },
@@ -19,9 +19,9 @@ const Questions = [{
     id: 1,
     q: "What is the name of Yasiin Bey’s character in Bamboozled?",
     a: [{ text: "Masta Ace", isCorrect: false },
-        { text: "Leon", isCorrect: false },
-        { text: "Craig", isCorrect: false },
-        { text: "Big Black Africa", isCorrect: true }
+    { text: "Leon", isCorrect: false },
+    { text: "Craig", isCorrect: false },
+    { text: "Big Black Africa", isCorrect: true }
     ]
 
 },
@@ -29,9 +29,9 @@ const Questions = [{
     id: 2,
     q: "What is the name of the pizzeria Mookie worked at in Do The Right Thing?",
     a: [{ text: "Tino's Pizza", isCorrect: false },
-        { text: "Sal's Famous", isCorrect: true },
-        { text: "Pizza Hut", isCorrect: false },
-        { text: "Buger King", isCorrect: false }
+    { text: "Sal's Famous", isCorrect: true },
+    { text: "Pizza Hut", isCorrect: false },
+    { text: "Buger King", isCorrect: false }
     ]
 
 },
@@ -39,9 +39,9 @@ const Questions = [{
     id: 3,
     q: "David Byrne and Spike Lee collaborated on what live filmed version of a broadway hit?",
     a: [{ text: "Annie", isCorrect: false, isSelected: false },
-        { text: "West Side Story", isCorrect: false },
-        { text: "American Utopia", isCorrect: true },
-        { text: "Bigger Than Life!", isCorrect: false }
+    { text: "West Side Story", isCorrect: false },
+    { text: "American Utopia", isCorrect: true },
+    { text: "Bigger Than Life!", isCorrect: false }
     ]
 
 },
@@ -49,9 +49,9 @@ const Questions = [{
     id: 4,
     q: "Who is the actor that plays the main character in Clockers?",
     a: [{ text: "Mekhi Phifer", isCorrect: true },
-        { text: "Omar Epps", isCorrect: false },
-        { text: "2 Pac", isCorrect: false },
-        { text: "Cormega", isCorrect: false }
+    { text: "Omar Epps", isCorrect: false },
+    { text: "2 Pac", isCorrect: false },
+    { text: "Cormega", isCorrect: false }
     ]
 
 },
@@ -59,9 +59,9 @@ const Questions = [{
     id: 5,
     q: "Who plays West Indian Archie in Malcolm X?",
     a: [{ text: "Charles S. Dutton", isCorrect: false },
-        { text: "Spike Lee", isCorrect: false },
-        { text: "Bill Duke", isCorrect: false },
-        { text: "Delroy Lindo", isCorrect: true }
+    { text: "Spike Lee", isCorrect: false },
+    { text: "Bill Duke", isCorrect: false },
+    { text: "Delroy Lindo", isCorrect: true }
     ]
 
 },
@@ -69,9 +69,9 @@ const Questions = [{
     id: 6,
     q: "Actress that playes “Natural” in 25th hour?",
     a: [{ text: "Ruby Dee", isCorrect: false },
-        { text: "Chloe Sevigny", isCorrect: false },
-        { text: "Rosario Dawson", isCorrect: true },
-        { text: "Angelina Jolie", isCorrect: false }
+    { text: "Chloe Sevigny", isCorrect: false },
+    { text: "Rosario Dawson", isCorrect: true },
+    { text: "Angelina Jolie", isCorrect: false }
     ]
 
 },
@@ -79,9 +79,9 @@ const Questions = [{
     id: 7,
     q: "Actor that plays Dean Big Brother Almighty in School Daze?",
     a: [{ text: "Chris Rock", isCorrect: false },
-        { text: "Giancarlo Esposito", isCorrect: true },
-        { text: "Samuel L. Jackson", isCorrect: false },
-        { text: "Eddie Murphy", isCorrect: false }
+    { text: "Giancarlo Esposito", isCorrect: true },
+    { text: "Samuel L. Jackson", isCorrect: false },
+    { text: "Eddie Murphy", isCorrect: false }
     ]
 
 },
@@ -89,9 +89,9 @@ const Questions = [{
     id: 8,
     q: "What movie did Spike Lee win an oscar for Best Adapted Screenplay?",
     a: [{ text: "Girl 6", isCorrect: false, isSelected: false },
-        { text: "Driving Miss Daisy", isCorrect: false },
-        { text: "Get On The Bus", isCorrect: false },
-        { text: "BlacKkKlansman", isCorrect: true }
+    { text: "Driving Miss Daisy", isCorrect: false },
+    { text: "Get On The Bus", isCorrect: false },
+    { text: "BlacKkKlansman", isCorrect: true }
     ]
 
 },
@@ -99,9 +99,9 @@ const Questions = [{
     id: 9,
     q: "What was Spike Lee's first movie?",
     a: [{ text: "She Hate Me", isCorrect: false, isSelected: false },
-        { text: "Kids", isCorrect: false },
-        { text: "She's Gotta Have It", isCorrect: true },
-        { text: "Drop Squad", isCorrect: false }
+    { text: "Kids", isCorrect: false },
+    { text: "She's Gotta Have It", isCorrect: true },
+    { text: "Drop Squad", isCorrect: false }
     ]
 
 },
@@ -109,9 +109,9 @@ const Questions = [{
     id: 10,
     q: "Spike Lee is from ____ ?",
     a: [{ text: "Jersey City", isCorrect: false, isSelected: false },
-        { text: "Chicago", isCorrect: false },
-        { text: "East Lansing", isCorrect: false },
-        { text: "Brooklyn", isCorrect: true }
+    { text: "Chicago", isCorrect: false },
+    { text: "East Lansing", isCorrect: false },
+    { text: "Brooklyn", isCorrect: true }
     ]
 
 }];
@@ -122,91 +122,91 @@ let start = true;
 // Iterate
 function iterate(id) {
 
-	// Getting the result display section
-	const result = document.getElementsByClassName("result");
-	result[0].innerText = "";
+    // Getting the result display section
+    const result = document.getElementsByClassName("result");
+    result[0].innerText = "";
 
-	// Getting the question
-	const question = document.getElementById("question");
-
-
-	// Setting the question text
-	question.innerText = Questions[id].q;
-
-	// Getting the options
-	const op1 = document.getElementById('op1');
-	const op2 = document.getElementById('op2');
-	const op3 = document.getElementById('op3');
-	const op4 = document.getElementById('op4');
+    // Getting the question
+    const question = document.getElementById("question");
 
 
-	// Providing option text
-	op1.innerText = Questions[id].a[0].text;
-	op2.innerText = Questions[id].a[1].text;
-	op3.innerText = Questions[id].a[2].text;
-	op4.innerText = Questions[id].a[3].text;
+    // Setting the question text
+    question.innerText = Questions[id].q;
 
-	// Providing the true or false value to the options
-	op1.value = Questions[id].a[0].isCorrect;
-	op2.value = Questions[id].a[1].isCorrect;
-	op3.value = Questions[id].a[2].isCorrect;
-	op4.value = Questions[id].a[3].isCorrect;
+    // Getting the options
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
 
-	let selected = "";
 
-	// Show selection for op1
-	op1.addEventListener("click", function (e) {
-		op1.style.backgroundColor = "lightgoldenrodyellow";
-		op2.style.backgroundColor = "lightskyblue";
-		op3.style.backgroundColor = "lightskyblue";
-		op4.style.backgroundColor = "lightskyblue";
-		selected = op1.value;
-	})
+    // Providing option text
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
 
-	// Show selection for op2
-	op2.addEventListener("click", function (e) {
-		op1.style.backgroundColor = "lightskyblue";
-		op2.style.backgroundColor = "lightgoldenrodyellow";
-		op3.style.backgroundColor = "lightskyblue";
-		op4.style.backgroundColor = "lightskyblue";
-		selected = op2.value;
-	})
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
 
-	// Show selection for op3
-	op3.addEventListener("click", function (e) {
-		op1.style.backgroundColor = "lightskyblue";
-		op2.style.backgroundColor = "lightskyblue";
-		op3.style.backgroundColor = "lightgoldenrodyellow";
-		op4.style.backgroundColor = "lightskyblue";
-		selected = op3.value;
-	})
+    let selected = "";
 
-	// Show selection for op4
-	op4.addEventListener("click", function (e) {
-		op1.style.backgroundColor = "lightskyblue";
-		op2.style.backgroundColor = "lightskyblue";
-		op3.style.backgroundColor = "lightskyblue";
-		op4.style.backgroundColor = "lightgoldenrodyellow";
-		selected = op4.value;
-	})
+    // Show selection for op1
+    op1.addEventListener("click", function (e) {
+        op1.style.backgroundColor = "lightgoldenrodyellow";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op1.value;
+    })
 
-	// grabbing the submit button
-	const evaluate = document.getElementsByClassName("submit");
+    // Show selection for op2
+    op2.addEventListener("click", function (e) {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightgoldenrodyellow";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op2.value;
+    })
 
-	// the submit method
-	evaluate[0].addEventListener("click", function (e) {
-		if (selected == "true") {
-			result[0].innerHTML = "Yup!  Click next question!";
-			result[0].style.color = "green";
-		} else {
-			result[0].innerHTML = "Nope, try again!";
-			result[0].style.color = "red";
-		}
-	})
+    // Show selection for op3
+    op3.addEventListener("click", function (e) {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightgoldenrodyellow";
+        op4.style.backgroundColor = "lightskyblue";
+        selected = op3.value;
+    })
+
+    // Show selection for op4
+    op4.addEventListener("click", function (e) {
+        op1.style.backgroundColor = "lightskyblue";
+        op2.style.backgroundColor = "lightskyblue";
+        op3.style.backgroundColor = "lightskyblue";
+        op4.style.backgroundColor = "lightgoldenrodyellow";
+        selected = op4.value;
+    })
+
+    // grabbing the submit button
+    const evaluate = document.getElementsByClassName("submit");
+
+    // the submit method
+    evaluate[0].addEventListener("click", function (e) {
+        if (selected == "true") {
+            result[0].innerHTML = "Yup!  Click next question!";
+            result[0].style.color = "green";
+        } else {
+            result[0].innerHTML = "Nope, try again!";
+            result[0].style.color = "red";
+        }
+    })
 }
 
 if (start) {
-	iterate("0");
+    iterate("0");
 }
 
 // Next button and method
@@ -214,18 +214,21 @@ const next = document.getElementsByClassName('next')[0];
 let id = 0;
 
 next.addEventListener("click", function (e) {
-	start = false;
-	if (id < 12) {
-		id++;
-		iterate(id);
-		console.log(id);
-	}
+    start = false;
+    if (id < 12) {
+        id++;
+        iterate(id);
+        console.log(id);
+    }
 })
 
 
 // makes a black box in the results box.
 
-let resultElement = document.querySelector('.result');
-resultElement.style.backgroundColor = 'black';
+// let resultElement = document.querySelector('.result');
+// resultElement.style.backgroundColor = 'black';
+
+// let questionElement = document.querySelector('.question-container');
+// questionElement.style.backgroundColor = 'green';
 
 // when timer expires or all questions are answered show a final score page & play again button that starts the whole process over again.
